587ce10438bc9524125e349dcc8365ea8a6e5d73
Eugene Chekanskiy <echekanskiy@gmail.com>
2018-05-03 18:58:53 +0300

    Initial

---
+++ server/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# temp variables
+INTERMEDIATE_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+INTERMEDIATE_IMAGE=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 32 | head -n 1)
+# configuration options
+AMBARI_REPO=http://s3.amazonaws.com/dev.hortonworks.com/ambari/centos7/2.x/BUILDS/2.7.0.0-439/ambaribn.repo
+IMAGE=ambari/hdf:3.2.0.0-270
+RESULT_TAG=2.7.0.0-439
+
+docker build --build-arg IMAGE=$IMAGE --build-arg AMBARI_REPO=$AMBARI_REPO -t $INTERMEDIATE_IMAGE .
+docker run --privileged --name $INTERMEDIATE_NAME -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 8080:8080 -h ambari.server -d $INTERMEDIATE_IMAGE && \
+docker exec -it $INTERMEDIATE_NAME bash -c "postgresql-setup initdb && systemctl enable postgresql && systemctl start postgresql && ambari-server setup -s && systemctl enable ambari-server" && \
+docker stop $INTERMEDIATE_NAME && \
+docker commit $INTERMEDIATE_NAME ambari/server:$RESULT_TAG
+docker rm $INTERMEDIATE_NAME
+docker rmi $INTERMEDIATE_IMAGE
